e70d8629fa695b0d76791617210dbe25928d4610
Arif6474 <arifulislam64743@gmail.com>
2022-02-17 22:02:54 +0600

    changed

--- money.js
+++ money.js
@@ -1,1 +1,35 @@
-console.log('my family');
+
+document.getElementById('culculation').addEventListener('click',function(){
+    // user food expense
+    const foodExpense = document.getElementById('food-expense');
+    const foodExpenseText = foodExpense.value;
+    const foodExpenseAmount = parseInt(foodExpenseText);
+    // user rent expense 
+    const rentExpense = document.getElementById('rent-expense');
+    const rentExpenseText = rentExpense.value;
+    const rentExpenseAmount = parseInt(rentExpenseText);
+    // user cloth expense 
+    const clothExpense = document.getElementById('cloth-expense');
+    const clothExpenseText = clothExpense.value;
+    const clothExpenseAmount = parseInt(clothExpenseText);
+    
+    const totalExpenses = document.getElementById('total-expenses');
+    const totalExpensesText = totalExpenses.value;
+    const totalExpensesAmount = parseInt(totalExpensesText);
+
+   // user total cost 
+   const totalCost = foodExpenseAmount + rentExpenseAmount + clothExpenseAmount;
+
+   totalExpenses.innerText = totalCost;
+
+   // user income
+   const incomeMonthly = document.getElementById('income');
+   const incomeAmount = parseInt(incomeMonthly.value);
+   const balanceRemain = document.getElementById('balance');
+   const balanceRemainAmount = parseInt(balanceRemain);
+   const balanceAmount = incomeAmount - totalCost;
+
+   balanceRemain.innerText = balanceAmount;
+
+  
+})
